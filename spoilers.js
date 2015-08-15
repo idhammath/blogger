25bd4e19dab7d5e20c07b359a5d944631a022a67
idhammath <idham.math@gmail.com>
2015-08-15 21:11:54 +0700

    Update spoilers.js

--- spoilers.js
+++ spoilers.js
@@ -1,13 +1,8 @@
-var arr = [];
-for(var jj = 0; jj &lt; 20; jj++) {
-arr[jj]="#spoiler"+String(jj+1);
-};
-  $(function() {
-for(var ii = 0; ii &lt; 20; ii++) {
-  	$( arr[ii] ).accordion({
-  			collapsible: true,
-  			active: true,
+$(function() {
+$( ".spoiler" ).accordion({
+      collapsible: true,
+active: true,
 			heightStyle: "content"
-  		});
-  };
+    });
+
   });
