6e8d17b2a6ea0a3fbafae102349841038224af23
idhammath <idham.math@gmail.com>
2015-08-15 19:53:31 +0700

    Create spoilers.js

---
+++ spoilers.js
@@ -0,0 +1,18 @@
+$(function() {
+    $( "spoiler" ).accordion({
+      collapsible: true
+    });
+  });
+  var array=[];
+  for(var ii = 0; ii < 10; ii++) {
+  	array[ii] = "spoiler"+String(ii+1);
+  };
+  $(function() {
+  	for(var jj = 0; jj < 10; jj++) {
+  		$( array[jj] ).accordion({
+  			collapsible: true,
+  			active: true,
+			heightStyle: "content"
+  		});
+  	};
+  });
