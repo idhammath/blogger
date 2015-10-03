6fe8bc2160be270fb96a2e91740e8a63a7f5f8fc
idhammath <idham.math@gmail.com>
2015-10-03 09:14:23 +0700

    Create smiley.js

---
+++ smiley.js
@@ -0,0 +1,37 @@
+/*
+* SEO Ciyus blog
+* url:seociyus.blogspot.com
+*Thanks to
+* Admin SEO Ciyus for this awesome emo comment thread
+ * bY SEO Ciyus
+ * Url:http://seociyus.blogspot.net
+ */ 
+a=document.getElementById("comment-holder");
+if(a){
+	b=a.getElementsByTagName("p");
+	for(i=0;i<b.length;i++){
+		if(b.item(i).getAttribute("CLASS")=="comment-content"){
+			_sc =b.item(i).innerHTML.replace(/:ahaa/gi, "<img src='http://1.bp.blogspot.com/-okT2YW2xLdk/UO8kORszupI/AAAAAAAABiA/-m2KzdtWy9g/s1600/seociyus.blogspot.com_052.gif' alt='' class='smiley'/>");
+ _sc = _sc.replace(/:baaa/gi, "<img src='http://3.bp.blogspot.com/-y0kyN7mxtSA/UO8kOPJf0KI/AAAAAAAABiE/KSpycJ1MxF8/s1600/seociyus.blogspot.com_051.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:caaa/gi, "<img src='http://4.bp.blogspot.com/-j06i2xh_8sM/UO8kObpX4rI/AAAAAAAABiI/PajlFXUQAmE/s1600/seociyus.blogspot.com_053.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:daaa/gi, "<img src='http://4.bp.blogspot.com/--lk67H6IWf4/UO8kPCv8fLI/AAAAAAAABiM/b4XAUwGmEbU/s1600/seociyus.blogspot.com_055.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:eaaa/gi, "<img src='http://1.bp.blogspot.com/-YKv8DELCCCE/UO8kPtGZSHI/AAAAAAAABiQ/yc1b2zT6gck/s1600/seociyus.blogspot.com_057.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:faaa/gi, "<img src='http://4.bp.blogspot.com/-iUBAaU71lDY/UO8kPfyuleI/AAAAAAAABiU/09SIIAD0J18/s1600/seociyus.blogspot.com_056.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:gaaa/gi, "<img src='http://2.bp.blogspot.com/-SnyOak6n5Ag/UO8kQXDwCgI/AAAAAAAABig/oMHXVC1XVzw/s1600/seociyus.blogspot.com_058.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:haaa/gi, "<img src='http://1.bp.blogspot.com/-k-HCrYQC4ZI/UO8kQufPrjI/AAAAAAAABik/ilRY6xFv57s/s1600/seociyus.blogspot.com_1.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:iaaa/ig, "<img src='http://2.bp.blogspot.com/-Zcpc13NqVqs/UO8kQrEz02I/AAAAAAAABio/K7ZtMjA6AEA/s1600/seociyus.blogspot.com_2.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:jaaa/gi, "<img src='http://3.bp.blogspot.com/-4ARnUQNOWto/UO8kRfhPofI/AAAAAAAABi0/_uev36cQ7xA/s1600/seociyus.blogspot.com_4.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:kaaa/gi, "<img src='http://4.bp.blogspot.com/-yHZsoA-TFd8/UO8kRpilcAI/AAAAAAAABi4/xGKkq5hlLlI/s1600/seociyus.blogspot.com_5.gif' alt='' class='smiley'/>");
+ _sc = _sc.replace(/:laaa/gi, "<img src='http://3.bp.blogspot.com/-Ci5V8FILdUI/UO8DG03KBmI/AAAAAAAABf4/vJwiwEe--Mw/s1600/seociyus.blogspot.com_070.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:maaa/gi, "<img src='http://3.bp.blogspot.com/-kdvLM-CxY64/UO8DqjtSCFI/AAAAAAAABgA/zSeaYBuX9co/s1600/seociyus.blogspot.com_010.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:naaa/gi, "<img src='http://3.bp.blogspot.com/-5sG0JNw1j8M/UO8ESkVJx_I/AAAAAAAABgI/1cRKvfWdzv0/s1600/seociyus.blogspot.com_059.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:oaaa/gi, "<img src='http://1.bp.blogspot.com/-R1SaeYx1eEc/UO8ES9f3ugI/AAAAAAAABgM/wmEMF82TkNQ/s1600/seociyus.blogspot.com_088.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:paaa/gi, "<img src='http://3.bp.blogspot.com/-vg-COD3Lj7U/UO8hm4yLiNI/AAAAAAAABhY/vnsCsvmREgs/s1600/seociyus.blogspot.com_012.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:qaaa/gi, "<img src='http://4.bp.blogspot.com/-mFn-qlR8URw/UO8hm7gw4YI/AAAAAAAABhc/Uob5CuPK3q8/s1600/seociyus.blogspot.com_033.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:raaa/gi, "<img src='http://3.bp.blogspot.com/-_kKLZ4YR9us/UO8hm-aYEJI/AAAAAAAABhg/UDJpVrogVug/s1600/seociyus.blogspot.com_011.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:saaa/gi, "<img src='http://4.bp.blogspot.com/-JCua6HjsXxk/UO8oKDxI6QI/AAAAAAAABjo/ekhBM06YrrE/s1600/seociyus.blogspot.com_7.gif' alt='' class='smiley'/>"); 
+ _sc = _sc.replace(/:taaa/gi, "<img src='http://3.bp.blogspot.com/-khk4bcrcL9g/UO8oKBU6xiI/AAAAAAAABjs/rwwl9KFgQYM/s1600/seociyus.blogspot.com_8.gif' alt='' class='smiley'/>"); 
+ b.item(i).innerHTML = _sc
+ }
+ } 
+ };
