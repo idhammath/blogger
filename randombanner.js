d0df0bad5b64020786b84f665b25f02ce0f06004
idhammath <idham.math@gmail.com>
2017-09-12 07:50:22 +0700

    Create randombanner.js

---
+++ randombanner.js
@@ -0,0 +1,3 @@
+var tes =[["http://bit.ly/2wlv4yu","https://4.bp.blogspot.com/-ttMSvvFoVCU/WbcWv1vHNzI/AAAAAAAAAJU/AEmnvS52D8Ejp7tBw_m9hhkapyIwyF7XgCLcBGAs/s1600/728.png"],["http://bit.ly/2wTHJwT","https://2.bp.blogspot.com/-uhW23JV1zeI/Wbcnwwfm_JI/AAAAAAAAAJk/u-KwNxkapcw8JOQoK01avKhd57_LvdQiQCLcBGAs/s1600/UC72890.png"]];
+
+$("#tempatbanner").html("<a href='"+tes[Math.floor(Math.random()*tes.length)][0]+"'><img src='"+tes[Math.floor(Math.random()*tes.length)][1]+"'/></a>");
