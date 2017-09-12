444ea4d305932b8ebaf034ac4264d0854af7e868
idhammath <idham.math@gmail.com>
2017-09-12 07:56:31 +0700

    Update randombanner.js

--- randombanner.js
+++ randombanner.js
@@ -1,3 +1,3 @@
-var tes =[["http://bit.ly/2wlv4yu","https://4.bp.blogspot.com/-ttMSvvFoVCU/WbcWv1vHNzI/AAAAAAAAAJU/AEmnvS52D8Ejp7tBw_m9hhkapyIwyF7XgCLcBGAs/s1600/728.png"],["http://bit.ly/2wTHJwT","https://2.bp.blogspot.com/-uhW23JV1zeI/Wbcnwwfm_JI/AAAAAAAAAJk/u-KwNxkapcw8JOQoK01avKhd57_LvdQiQCLcBGAs/s1600/UC72890.png"]];
+var daftariklan =[["http://bit.ly/2wlv4yu","https://4.bp.blogspot.com/-ttMSvvFoVCU/WbcWv1vHNzI/AAAAAAAAAJU/AEmnvS52D8Ejp7tBw_m9hhkapyIwyF7XgCLcBGAs/s1600/728.png"],["http://bit.ly/2wTHJwT","https://2.bp.blogspot.com/-uhW23JV1zeI/Wbcnwwfm_JI/AAAAAAAAAJk/u-KwNxkapcw8JOQoK01avKhd57_LvdQiQCLcBGAs/s1600/UC72890.png"],["http://bit.ly/2xsOxCi","https://4.bp.blogspot.com/-Q84hceISe_g/WbcwPaE5oyI/AAAAAAAAAJ0/RkehyyAbEaYpEkc93afILiyo0qhAVop0wCLcBGAs/s1600/iklanflai.gif"];
 
-$("#tempatbanner").html("<a href='"+tes[Math.floor(Math.random()*tes.length)][0]+"'><img src='"+tes[Math.floor(Math.random()*tes.length)][1]+"'/></a>");
+$("#tempatbanner").html("<a href='"+daftariklan[Math.floor(Math.random()*daftariklan.length)][0]+"'><img src='"+daftariklan[Math.floor(Math.random()*daftariklan.length)][1]+"'/></a>");
