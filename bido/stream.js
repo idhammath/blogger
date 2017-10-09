de824bb244f25edc3cc5735899f6808056fc51db
idhammath <idham.math@gmail.com>
2017-10-09 13:24:41 +0700

    Update stream.js

--- bido/stream.js
+++ bido/stream.js
@@ -1,7 +1,7 @@
 var liskode = [];
 liskode[0]="<iframe width='595' height='360' src='https://uptostream.com/iframe/ct57f3vm7y9x' scrolling='no' frameborder='0' allowfullscreen webkitallowfullscreen></iframe>";
 liskode[1]="<iframe src='https://openload.co/embed/MeDeqd52bRM/Kingsman%2520The%2520Golden%2520Circle%25202017%2520TS%2520Xvid%2520AC3.avi.mp4' scrolling='no' frameborder='0' width='700' height='430' allowfullscreen='true' webkitallowfullscreen='true' mozallowfullscreen='true'></iframe>";
-document.getElementById("tempatstream").innerHTML=liskode[3];
+document.getElementById("tempatstream").innerHTML=liskode[0];
 var isiserver = "";
 var hehe;
 var antri;
