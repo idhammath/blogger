920dd1f5e088ec167fe7f2eddfbd91db00e1c21e
idhammath <idham.math@gmail.com>
2017-10-27 09:58:13 +0700

    Create macromediaflash8pro.js

---
+++ bido/macromediaflash8pro.js
@@ -0,0 +1,104 @@
+var j;
+var i;
+var k;
+var teks;
+var ling = [
+["Faststore","https://faststore.org/cudgygtjosf1"],
+["Keepshare","https://keepshare.net/zk68oq5cfmo3/Macromedia_Flash_8_Professional.zip"],
+["Uptobox","http://uptobox.com/ieehx9qs1i2v"],
+["Userscloud","https://userscloud.com/8krqact8bcfr"],
+["SendIt Cloud","https://sendit.cloud/lw5rl23x16zt"],
+["XeUpload","http://xeupload.com/7d6gsz933j0w"],
+["UltimateDown",""],
+["Openload","https://oload.stream/f/aeq7QT7AxmI"],
+["Uploads.to","https://uploads.to/e5ov1xei8wdy"],
+["Uplod.cc","https://uplod.cc/u52iytq9uf11"],
+["DoUploads","http://douploads.com/d/CrA"],
+["Upload.mn","https://upload.mn/6kkh7gl9ew9c"],
+["Clickn Upload","https://clicknupload.org/a78l397r6kab"],
+["FileFlares","http://fileflares.com/yokuxpxs48zn/Macromedia_Flash_8_Professional.zip.html"],
+["Upload Ocean",""],
+["BDupload","https://bdupload.info/5gzm6bw1uh3p"],
+["UploadX","https://uploadx.link/pkxnk4q0zysi"],
+["Indishare",""],
+["File Factory","http://filefactory.com/file/ljlnlqto5q1/Macromedia%2520Flash%25208%2520Professional.zip"],
+["Oboom","https://www.oboom.com/8Z5D1K32"],
+["4DownFiles","https://4downfiles.org/al8hk2h1inr5"],
+["UPloadboy","http://uploadboy.me/3xx8s1uibp88/Macromedia%20Flash%208%20Professional.zip.html"],
+["Files CDN",""],
+["Drop APK","https://dropapk.com/5bohd8joy2d3"],
+["Bank Upload","https://dropapk.com/5bohd8joy2d3"],
+["Bestream.tv","http://bestream.tv/a5jS/Macromedia-Flash-8-Professional.html"],
+["Flashx.tv",""],
+["Vidlox.tv",""],
+["Streamango",""],
+["Streamcherry",""],
+["Vidoza",""],
+["Clip Watching",""],
+["Rapid Video",""],
+["eStream",""],
+["Vodlock",""],
+["Vidto.me",""],
+["Thevideo",""],
+["Watchers",""],
+["Streamin",""],
+["Videowood",""],
+["Vidup",""],
+["VidToDo",""],
+["Youwatch",""],
+["Google Drive","https://goo.gl/wyV2At"]
+];
+var idtombol = ["faststore","keepshare","uptobox","userscloud","senditcloud","xeupload","ultimatedown","openload","uploadsto","uplodws","douploads","uploadmn","clicknupload","fileflares","uploadocean","bdupload","uploadx","indishare","filefactory","oboom","fourdownfiles","uploadboy","filescdn","dropapk","bankupload","bestream","flashxtv","vidloxtv","streamango","streamcherry","vidoza","clipwatching","rapidvideo","estream","vodlock","vidtome","thevideo","watchers","streamin","videowood","vidup","vidtodo","youwatch","googledrive"];
+var idtombol2 = [];
+for (k=0;k<idtombol.length;k++){
+	idtombol2[k]="#"+idtombol[k];
+}
+$(document).ready(function(){
+  $("#tempattombol").hide();
+  var awal = 5;
+  var kondon = setInterval(function(){fungtime()},1000);
+  function fungtime(){
+    $("#angkatimer").html("Please wait...<br><div style='font-size: 400%;'>"+awal+"</div>");
+    awal-=1;
+    if (awal==-2) {
+      clearInterval(kondon);
+      $("#angkatimer").hide();
+      $("#tombolgetlink").removeClass("btn-secondary");
+      $("#tombolgetlink").addClass("btn-primary");
+      $("#tombolgetlink").removeAttr("disabled");
+    }
+  };
+
+for (j=0;j < idtombol.length; j++){
+$("#tempattombol").append("<a id='"+idtombol[j]+"' class='btn btn-primary btn-lg' href='' role='button' style='margin-bottom:0.3rem'>"+ling[j][0]+"</a> ");
+};
+for (i = 0; i < idtombol.length; i++) {
+if (ling[i][1]==""){
+$(idtombol2[i]).addClass("btn-secondary disabled");
+$(idtombol2[i]).attr("hidden","hidden");
+}else{
+    $(idtombol2[i]).attr("href",ling[i][1]);
+};
+};
+});
+function muncultombol(){
+  $("#tempattombol").show();
+  $("#tombolgetlink").hide();
+};
+</script>
+</head>
+<body>
+<!-- First Container -->
+<div class="container-fluid bg-info text-center">
+<script type="text/javascript" src="//ylx-1.com/bnr.php?section=General&pub=996325&format=728x90&ga=g"></script>
+<noscript><a href="https://yllix.com/publishers/996325" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/728x90.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" /></a></noscript>
+<p id="tempatiklanhori"></p>
+<p id="tempatiklanpersegi"></p>
+<!-- START AD CODE -->
+<script language="javascript" type="text/javascript" charset="utf-8">
+var cpxcenter1522_banner_border = '#990033';
+var cpxcenter1522_banner_text = '#000000';
+var cpxcenter1522_banner_bg = '#FFFFFF';
+cpxcenter1522_banner_link = '#000000';
+cpxcenter1522_width = 250;
+cpxcenter1522_height = 250;
