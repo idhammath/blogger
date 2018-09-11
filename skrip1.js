9effc2e78df52948e56b85b9a7e8610f727cd94d
idhammath <idham.math@gmail.com>
2018-09-11 12:03:07 +0700

    Create skrip1.js

---
+++ skrip1.js
@@ -0,0 +1,18 @@
+$("#pen2").hide();
+$(document).ready(function(){
+  $("#cek2").hide();
+  $(".sal2").click(function(){
+    $(".sal2").removeClass("sal2 btn-dark").addClass("btn-danger");
+    $("#cek2").html("SALAH!!");
+    $("#cek2").css("font-weight","bold");
+    $("#cek2").show();
+    $("#pen2").show();
+  });
+  $(".ben2").click(function(){
+    $(".ben2").removeClass("ben2 btn-dark").addClass("btn-success");
+    $("#cek2").html("BENAR!!");
+    $("#cek2").css("font-weight","bold");
+    $("#cek2").show();
+    $("#pen2").show();
+  });
+});
