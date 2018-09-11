d1d28e9aa5fea78587dc7839051322b47c3e4eb6
idhammath <idham.math@gmail.com>
2018-09-11 12:45:39 +0700

    Update skrip1.js

--- skrip1.js
+++ skrip1.js
@@ -1,13 +1,14 @@
 $("#pen2").hide();
 $(document).ready(function(){
   function testes(){
+     $(".sal2").off("click");
+    $(".ben2").off("click");
     $(".sal2").removeClass("sal2 btn-dark").addClass("btn-danger");
     $(".ben2").removeClass("ben2 btn-dark").addClass("btn-success");
     $("#cek2").css("font-weight","bold");
     $("#cek2").show();
     $("#pen2").show();
-    $(".sal2").off("click");
-    $(".ben2").off("click");
+   
   }
   $("#cek2").hide();
   $(".sal2").click(function(){
