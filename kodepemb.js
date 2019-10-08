3ec025415cbf142bb89a07bedb4aa2230caef984
idhammath <idham.math@gmail.com>
2019-10-09 06:37:41 +0700

    Update kodepemb.js

--- kodepemb.js
+++ kodepemb.js
@@ -1,5 +1,3 @@
-var kode=["TER081019"];
-var link=["https://matematikazone.id/2017/05/teorema-sisa-soal-dan-pembahasan.html#7"]
 $( "#tombol").click(function() {
   var kod=$("#isikode").val();
   var indkod=kode.indexOf(kod);
