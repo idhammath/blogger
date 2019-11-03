d851013be5f53526f5802d840cb153c647ccc027
idhammath <idham.math@gmail.com>
2019-11-03 12:08:05 +0700

    Update tryout.js

--- tryout.js
+++ tryout.js
@@ -4,7 +4,7 @@
       tes(this);
     }
   };
-  xmlhttp.open("GET", "https://cdn.statically.io/gh/idhammath/blogger/872d1ac4/tryout.xml", true);
+  xmlhttp.open("GET", "https://cdn.statically.io/gh/idhammath/blogger/f12590ff/tryout.xml", true);
   xmlhttp.send();
   function tes(xml) {
   var i;
