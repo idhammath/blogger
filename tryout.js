931fd525640902f0fc91b0a0fb729d31c626cef0
idhammath <idham.math@gmail.com>
2019-11-03 11:55:54 +0700

    Create tryout.js

---
+++ tryout.js
@@ -0,0 +1,16 @@
+	  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      tes(this);
+    }
+  };
+  xmlhttp.open("GET", "https://cdn.statically.io/gh/idhammath/blogger/846fc09e/tryout.xml", true);
+  xmlhttp.send();
+  function tes(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var x = xmlDoc.getElementsByTagName("nomor");
+  var isi=x[0].getElementsByTagName("soal")[1].childNodes[0].nodeValue;
+  document.getElementById("hehe").innerHTML = isi;
+  document.getElementById("byk").innerHTML = x[0].getElementsByTagName("soal").length;
+}
