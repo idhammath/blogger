65557a75534ee502af4322521fcdbc8cc2eb735c
idhammath <idham.math@gmail.com>
2021-12-29 20:44:57 +0700

    Create down.js

---
+++ ebookidham/down.js
@@ -0,0 +1,6 @@
+var down = document.getElementsByClassName("down");
+for(let i=0;i<down.length;i++){
+  down[i].onclick = function(){
+     window.open(`https://idhamdaz2.blogspot.com/p/safelink-2_27.html?${kode[i][0]}|${kode[i][1]}|${document.title}`,"_blank")
+  }
+}
